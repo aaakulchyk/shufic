510866f9b5e6376c5566bc434117663376383bec
laneboi <solid.formalin@gmail.com>
2018-11-04 16:46:43 +0300

    Rewrote show_all_videos() to show_latest_videos() as in documentation.

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,10 @@ from django.template.context_processors import csrf
 from django.contrib import auth
 
 
-def show_all_videos(request):
-    content = []
-    for video in Video.objects.all():
-        one_video = [video]
-        one_video.append(Comment.objects.filter(videoparent_id=video.id))
-        content.append(one_video)
-    return render(request, 'home.html', {"content": content, "username": auth.get_user(request).username})
+def show_latest_videos(request):
+    latest_videos_list = Video.objects.order_by('-date')[:10]
+    context = {'latest_videos_list': latest_videos_list,}
+    return render(request, 'home.html', context)
 
 
 def show_video(request, video_id):
@@ -40,15 +37,6 @@ def like_video(request, video_id):
     return redirect('/video/' + str(video_id) + '/')
 
 
-def like_video_ajax(request):
-    if request.GET:
-        idvideo = request.GET['like_video']
-        video = Video.objects.get(id=idvideo)
-        video.Video_likos += 1
-        video.save()
-    return HttpResponse(video.rating)
-
-
 def dislike_video(request, video_id):
     response = redirect('/video/' + str(video_id) + '/')
     if video_id in request.COOKIES:
@@ -58,14 +46,6 @@ def dislike_video(request, video_id):
         Video.objects.get(id=video_id).rating -= 1
         response.set_cookie(video_id, 'I don\'t like it!')
     return response
-
-
-def dislike_video_ajax(request):
-    if request.GET:
-        video = Video.objects.get(id=request.GET['dislike_video'])
-        video.rating -= 1
-        video.save()
-    return HttpResponse(video.rating)
 
 
 def commit(request, video_id):
