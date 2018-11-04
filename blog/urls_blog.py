bf4962670de01f6e71ef14f8d6543f77375a9e8f
laneboi <solid.formalin@gmail.com>
2018-11-04 17:10:01 +0300

    Added app_name to urls_blog.py.

--- blog/urls_blog.py
+++ blog/urls_blog.py
@@ -2,13 +2,14 @@ from django.urls import re_path
 from . import views
 
 
+app_name = 'blog'
 urlpatterns = [
-    re_path(r'^$', views.show_all_videos),
-    re_path(r'^bio$', views.show_bio),
+    re_path(r'^$', views.show_latest_videos),
+    re_path(r'^bio$', views.show_bio, name='bio'),
     re_path(r'^video/(?P<video_id>\d+)/$', views.show_video),
     re_path(r'^like_video/(?P<video_id>\d+)/$', views.like_video),
-    re_path(r'^like_video/like_video_ajax/$', views.like_video_ajax),
+    #re_path(r'^like_video/like_video_ajax/$', views.like_video_ajax),
     re_path(r'^dislike_video/(?P<video_id>\d+)/$', views.dislike_video),
-    re_path(r'^dislike_video/dislike_video_ajax/$', views.dislike_video_ajax),
+    #re_path(r'^dislike_video/dislike_video_ajax/$', views.dislike_video_ajax),
     re_path(r'^commit/(?P<video_id>\d+)/$', views.commit),
 ]
