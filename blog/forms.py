08cac131148a08d4daa10fb4868482b10db5ac20
laneboi <solid.formalin@gmail.com>
2018-11-05 00:09:29 +0300

    Finally fixed CommentForm.

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm
-from . import models
+from django.forms import ModelForm, Textarea
+from django.utils.translation import gettext_lazy as _
+from .models import Comment
 
 
 class CommentForm(ModelForm):
     class Meta:
-        model = models.Comment
+        model = Comment
         fields = ['text']
+        widgets = {
+            'text': Textarea(attrs={'rows': 2, 'class': 'form-control'}),
+        }
+        labels = {
+            'text': _('Комментировать'),
+        }
