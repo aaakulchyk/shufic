e28b00d65c32d8f73a9f02a149afb1dce732ec93
laneboi <solid.formalin@gmail.com>
2018-11-04 16:47:52 +0300

    Rewrote show_all_videos() to show_latest_videos() as in documentation.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Video, Comment
 from . import form
 from django.template.context_processors import csrf
